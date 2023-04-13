5aa21a13a81cc9bd903d304f73f170e2ab312a02
rickardpeters <77685817+rickardpeters@users.noreply.github.com>
2023-04-13 13:10:08 +1200

    testat runt lite med strava api. kan göra ett inloggningsanrop till strava och hämta data, behöver bara fixa med accesstoken och refreshtoken så kan man visa lite data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loggedInState, setLoggedInState] = useState(false)
+
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop:string) => searchParams.get(prop),
+  });
+
+  const handleClick = async () => {
+    window.location.href='https://www.strava.com/oauth/authorize?client_id=105576&redirect_uri=http://127.0.0.1:5173&response_type=code&scope=read'
+    setLoggedInState(!loggedInState)
+    console.log(loggedInState)
+  }
+
+  const fetchData = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    var code = ""
+    code = urlParams.get('code');
+    console.log(code)
+    console.log(getAccessToken(code))
+
+    const accessToken = await getAccessToken(code);
+    const refreshToken = response.data.refresh_token;
+
+  }
+
+
+  async function getAccessToken(code: string): Promise<string> {
+    try {
+      const response = await axios.post("https://www.strava.com/oauth/token?client_id=105576&client_secret=d91be7e7d6dc2775e6ee24f494d7079c172e2c8f&code="+code+"&grant_type=authorization_code"
+      );
+
+      const accessToken = response.data.access_token
+      return accessToken
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error getting access token from Strava API");
+    }
+  }
+
+
 
   return (
-    <div className="App">
+    <div>
+      <h1>
+        Springa på lite
+      </h1>
+      <button type='submit' onClick={handleClick}>{loggedInState === false ? "Sign in": "Sign out"}</button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button type='submit' onClick={fetchData}>Fetch</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
