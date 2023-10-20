e57d50ca2ed4c2828afb973b169238e79212044b
Martin Forsberg <martin-forsberg@hotmail.com>
2023-10-20 16:37:34 +0200

    authorize firebase login in backend, new app for firebase auth, added helper functions to UserExtended

--- server/backend/views.py
+++ server/backend/views.py
@@ -1,57 +1,62 @@
 from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate, login, logout
+from rest_framework.decorators import api_view
+from django.contrib.auth import  login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.authtoken.models import Token
+from firebaseAuth.authentication import FirebaseAuthentication
+
+from server.settings import AUTHENTICATION_BACKENDS
 from .serializers import UserSerializer, CommunitySerializer, ChallengeSerializer
-from .models import Community, Challenge
+from .models import Community, Challenge, UserExtended
 import json
 
-
 @csrf_exempt
+@api_view(["POST"])
 def log_in(request):
     if request.method == "POST":
-        data = json.loads(request.body.decode("utf-8"))
 
-        email = data["email"]
-        password = data["password"]
-
-        user = authenticate(username=email, password=password)
-
+        user, _ = FirebaseAuthentication().authenticate(request)
+        print(user)
         if user is not None:
-            login(request, user)
+            login(request, user.user)
             ser_user = UserSerializer(user)
-            return JsonResponse(ser_user.data, safe=False)
+            return Response(ser_user.data)
 
         else:
             return Response("User not found.", status=404)
 
-
+@api_view(["GET"])
+def getUsers(request):
+    users = User.objects.all()
+    return Response("User not found.", status=404)
 @csrf_exempt
+@api_view(["POST"])
 def log_out(request):
-    if request.method == "POST":
-        try:
-            user = User.objects.get(username=request.user.username)
-        except ObjectDoesNotExist:
-            return JsonResponse(status.HTTP_404_NOT_FOUND, safe=False)
-
+    user, created = FirebaseAuthentication().authenticate(request)
+    if user.is_authenticated :
+        
         logout(request)
         return JsonResponse(status.HTTP_200_OK, safe=False)
+    else:
+        return JsonResponse(status.HTTP_404_NOT_FOUND, safe=False)
+
+        
 
 
 @csrf_exempt
 @login_required
+@api_view(["GET"])
 def communities(request):
-    if request.method == "GET":
-        communities = Community.objects.all()
+    communities = Community.objects.all()
 
-        if not communities:
-            return HttpResponse("No communities found.")
+    if not communities:
+        return HttpResponse("No communities found.")
 
-        ser_communities = CommunitySerializer(communities, many=True)
+    ser_communities = CommunitySerializer(communities, many=True)
 
-        return JsonResponse(ser_communities.data, safe=False)
+    return JsonResponse(ser_communities.data, safe=False)
