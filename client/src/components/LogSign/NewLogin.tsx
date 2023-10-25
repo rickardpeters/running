adf85161208a9eb14e5f206a6bbc797dace6b017
Martin Forsberg <martin-forsberg@hotmail.com>
2023-10-25 16:38:32 +0200

    Made Login to fom so we can have a submit button

--- client/src/components/LogSign/NewLogin.tsx
+++ client/src/components/LogSign/NewLogin.tsx
@@ -50,30 +50,31 @@ const NewLogin = () => {
 
   return (
     <div className="grid place-items-center h-[45vh] relative">
-      <div className="grid m-2">
-        <input
-          className="input input-bordered mb-2"
-          id="password"
-          placeholder="E-mail"
-          value={email}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(e.target.value)
-          }
-        ></input>
-        <input
-          placeholder="Password"
-          className="input input-bordered"
-          type="password"
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        ></input>
-        <button className="btn btn-accent mt-5" onClick={logIn}>
-          Login
-        </button>
-      </div>
-      <div></div>
+      <form>
+        <div className="grid m-2">
+          <input
+            className="input input-bordered mb-2"
+            id="password"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
+          ></input>
+          <input
+            placeholder="Password"
+            className="input input-bordered"
+            type="password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+          ></input>
+          <button className="btn btn-accent mt-5" onClick={logIn} type="submit">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
