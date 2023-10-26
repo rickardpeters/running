dbe06a1c608396f966f1e889de90d2a288cbeaa6
Martin Forsberg <martin-forsberg@hotmail.com>
2023-10-26 12:55:44 +0200

    Login reqired for logout

--- server/backend/views.py
+++ server/backend/views.py
@@ -36,6 +36,7 @@ def getUsers(request):
     users = User.objects.all()
     return Response("User not found.", status=404)
 @csrf_exempt
+@login_required
 @api_view(["POST"])
 def log_out(request):
     user, created = FirebaseAuthentication().authenticate(request)
@@ -61,49 +62,8 @@ def communities(request):
     ser_communities = CommunitySerializer(communities, many=True)
 
     return JsonResponse(ser_communities.data, safe=False)
-from django.http import HttpResponse, JsonResponse
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.authtoken.models import Token
-from .serializers import UserSerializer, CommunitySerializer, ChallengeSerializer
-from .models import Community, Challenge
-import json
 
 
-@csrf_exempt
-def log_in(request):
-    if request.method == "POST":
-        data = json.loads(request.body.decode("utf-8"))
-
-        email = data["email"]
-        password = data["password"]
-
-        user = authenticate(username=email, password=password)
-
-        if user is not None:
-            login(request, user)
-            ser_user = UserSerializer(user)
-            return JsonResponse(ser_user.data, safe=False)
-
-        else:
-            return Response("User not found.", status=404)
-
-
-@csrf_exempt
-def log_out(request):
-    if request.method == "POST":
-        try:
-            user = User.objects.get(username=request.user.username)
-        except ObjectDoesNotExist:
-            return JsonResponse(status.HTTP_404_NOT_FOUND, safe=False)
-
-        logout(request)
-        return JsonResponse(status.HTTP_200_OK, safe=False)
 
 
 """
