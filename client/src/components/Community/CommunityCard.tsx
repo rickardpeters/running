e5b429af4969b32d7ca97cb0223e8bc401385320
Martin Forsberg <martin-forsberg@hotmail.com>
2023-10-26 13:12:04 +0200

    Some styling and link for community card component

--- client/src/components/Community/CommunityCard.tsx
+++ client/src/components/Community/CommunityCard.tsx
@@ -12,6 +12,8 @@ import { useRecoilState } from "recoil";
 import { showDeleteCommunityAtom } from "../../recoil/atoms";
 import DeleteCommunityModal from "./DeleteCommunityModal";
 import DeleteCommunityConfirmation from "./DeleteCommunityConfirmation";
+import "./comunityStyle.css";
+import { Link } from "react-router-dom";
 interface CommunityCardProps {
   community: Community;
 }
@@ -34,40 +36,42 @@ const CommunityCard = ({ community }: CommunityCardProps) => {
         minWidth: "300px",
       }}
     >
-      <Card sx={{ minWidth: 200, margin: "10px" }}>
-        <CardContent>
-          <Typography
-            sx={{ fontSize: 14 }}
-            color="text.secondary"
-            gutterBottom
-          ></Typography>
-          <Typography variant="h5" component="div">
-            {community.community_name}
-            {"      "}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
-          <Typography variant="body2">
-            {community.description === "" ? (
-              <i>No description</i>
-            ) : (
-              community.description
-            )}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant="contained" size="small">
-            Edit
-          </Button>
-          <Button
-            variant="contained"
-            color="error"
-            size="small"
-            onClick={() => handleDeleteClick(community)}
-          >
-            Delete
-          </Button>
-        </CardActions>
-      </Card>
+      <Link to={""}>
+        <Card sx={{ minWidth: 200, margin: "10px" }} className="community-card">
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            ></Typography>
+            <Typography variant="h5" component="div">
+              {community.community_name}
+              {"      "}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
+            <Typography variant="body2">
+              {community.description === "" ? (
+                <i>No description</i>
+              ) : (
+                community.description
+              )}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button variant="contained" size="small">
+              Edit
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              size="small"
+              onClick={() => handleDeleteClick(community)}
+            >
+              Delete
+            </Button>
+          </CardActions>
+        </Card>
+      </Link>
       <DeleteCommunityModal community={deleteCommunity} />
       <DeleteCommunityConfirmation />
     </Container>
