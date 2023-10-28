3c3636aaf925b9de694da746ffa07712ab77fc5d
rickardpeters <77685817+rickardpeters@users.noreply.github.com>
2023-10-28 16:14:23 +0200

    fixed an import

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,4 +1,3 @@
-import "../App.css";
 import { Container } from "@mui/material";
 import StravaCard from "../components/StravaCard";
 import ChallengeList from "../components/Challenge/ChallengeList";
