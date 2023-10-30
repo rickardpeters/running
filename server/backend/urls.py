a87fe15cafa6e4d991eb4ad6214974d4cde7ecdb
rickardpeters <77685817+rickardpeters@users.noreply.github.com>
2023-10-30 17:38:14 +0100

    general read and create for challenges created

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import join_community, log_in, log_out, communities, community_by_id
+from .views import (
+    join_community,
+    log_in,
+    log_out,
+    communities,
+    community_by_id,
+    challenges,
+)
 
 urlpatterns = [
     path("users/login/", log_in, name="login"),
     path("users/logout/", log_out, name="login"),
     path("communities/", communities, name="communities"),
     path("communities/<int:community_id>/", community_by_id, name="communities/id"),
-    path("communities/join/",join_community, name="join-community"),
+    path("communities/join/", join_community, name="join-community"),
+    path("challenges/", challenges, name="challenges"),
 ]
